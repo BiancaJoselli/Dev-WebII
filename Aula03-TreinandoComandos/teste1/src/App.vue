eb133513ada443a1d78df3f6dd6c88f47daa1a46
BiancaJoselli <biancajoselli.ifc@gmail.com>
2025-03-24 11:54:10 -0300

    FEAT: Treinamento de comandos

---
+++ Aula03-TreinandoComandos/teste1/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  <h1>Olá Mundo</h1>
+</template>
+
+<style scoped>
+</style>
---
+++ Aula03-TreinandoComandos/teste4/src/App.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h1>Olá Mundo!</h1>
+</template>
+
+<style scoped>
+</style>
