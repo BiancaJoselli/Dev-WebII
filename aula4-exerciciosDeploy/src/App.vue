df32e427c12c573cb06edac5245b3d1091a54728
BiancaJoselli <biancajoselli.ifc@gmail.com>
2025-03-25 09:17:17 -0300

    FEAT: Treinando deploy

---
+++ aula4-exerciciosDeploy/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const contador = ref(0)
+
+function decrementar() {
+  if (contador.value > 0) {
+    contador.value--
+  } else {
+    alert(`O contador não pode ser menor que zero!`)
+  }
+}
+
+function incrementar() {
+  contador.value++
+}
+</script>
+
+<template>
+  <h1>Meu contador</h1>
+  <p>Valor do contador: {{ contador }}</p>
+  <button v-on:click="incrementar">+</button>
+  <button @click="decrementar">-</button>
+</template>
