9df464cb95ee05fe65c03a9786a0ef42843baa12
BiancaJoselli <biancajoselli.ifc@gmail.com>
2025-04-08 09:27:05 -0300

    FEAT: Aprendendo Renderização de Listas

--- aula6-PropriendadesComputadas/Exercicio-PropriedadesComputadas/src/App.vue
+++ aula6-PropriendadesComputadas/Exercicio-PropriedadesComputadas/src/App.vue
@@ -1,42 +1,96 @@
 <script setup>
-  import { ref } from 'vue';
+import { computed, ref } from 'vue'
 
-  const professor = ref({
-    nome: 'Eduardo da Silva',
-    disciplinas: [
-      'Desenvolvimento Web II',
-      'Desenvolvimento de Aplicativos para Dispositivos Móveis',
-    ],
-  });
+const contador1 = ref(0)
+const contador2 = ref(0)
+const valorBooleano = ref(true)
 
-  function possuiDisciplinas() {
-    return professor.value.disciplinas.length > 0;
+function incrementar(contador) {
+  eval(contador).value++
+}
+
+function decrementar(contador) {
+  if (eval(contador).value > 0) {
+    eval(contador).value--
   }
-
-  function incrementar() {
-  contador.value++
 }
 
-function decrementar() {
-  if (contador.value > 0) {
-      contador.value--;
-    }
-}
-
+const soma = computed(() => contador1.value + contador2.value)
+const somaPar = computed(() => soma.value % 2 === 0)
+const somaMaiorQue10 = computed(() => soma.value > 10)
 </script>
 
 <template>
-  <div>
-    <h1>Professor</h1>
-    <p>Nome: {{ professor.nome }}</p>
-    <p v-if="!possuiDisciplinas">O professor não possui disciplinas.</p>
-    <div v-else>
-      <p>O professor possui disciplinas.</p>
-      <ul>
-        <li v-for="disciplina in professor.disciplinas" :key="disciplina">
-          {{ disciplina }}
-        </li>
-      </ul>
+  <h1>Correção dos exercícios</h1>
+  <div class="booleano">
+    <button @click="valorBooleano = !valorBooleano">
+      {{ valorBooleano ? 'Esconder Resultado' : 'Mostrar Resultado' }}
+    </button>
+  </div>
+  <div v-if="valorBooleano">
+    <div class="contador">
+      <h2>Contador 1</h2>
+      <button @click="incrementar('contador1')">Incrementar</button>
+      <button @click="decrementar('contador1')">Decrementar</button>
+      <p>Valor: {{ contador1 }}</p>
+    </div>
+
+    <div class="contador">
+      <h2>Contador 2</h2>
+      <button @click="incrementar('contador2')">Incrementar</button>
+      <button @click="decrementar('contador2')">Decrementar</button>
+      <p>Valor: {{ contador2 }}</p>
+    </div>
+
+    <div class="soma">
+      <h2>Soma</h2>
+
+      <p>Valor: {{ soma }}</p>
+
+      <div v-if="somaMaiorQue10">
+        <p>A soma é maior que 10</p>
+      </div>
+
+      <div v-else>
+        <p>A soma é menor que 10</p>
+      </div>
+
+      <div v-if="somaPar">
+        <p>A soma é par</p>
+      </div>
+
+      <div v-else>
+        <p>A soma é ímpar</p>
+      </div>
     </div>
   </div>
 </template>
+
+<style scoped>
+  
+h1 {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 2vw;
+  margin: 1vw 0.5vw;
+}
+
+h2 {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 1.5vw;
+  margin: 1vw 0.5vw;
+}
+
+p {
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 0.6vw 0.5vw;
+}
+
+button {
+  border: none;
+  padding: 1vw;
+  background-color: rgb(133, 133, 252);
+  color: black;
+  margin: 0.5vw;
+  border-radius: 1vw;
+}
+</style>
