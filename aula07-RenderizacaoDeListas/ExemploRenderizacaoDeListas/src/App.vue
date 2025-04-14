b9f96e7cd8d23c3ceb3a54bc36773e67cccce2a7
BiancaJoselli <biancajoselli.ifc@gmail.com>
2025-04-14 12:02:06 -0300

    FEAT: Realizando exercícios de Renderização de Listas

--- aula07-RenderizacaoDeListas/ExemploRenderizacaoDeListas/src/App.vue
+++ aula07-RenderizacaoDeListas/ExemploRenderizacaoDeListas/src/App.vue
@@ -1,10 +1,28 @@
 <script setup>
+const items = [
+    {
+    id: 1,
+    name: 'Item 1',
+    details: { description: 'Item 1 description', price: 10.0 },
+    },
+    {
+    id: 2,
+    name: 'Item 2',
+    details: { description: 'Item 2 description', price: 20.0 },
+    },
+    {
+    id: 3,
+    name: 'Item 3',
+    details: { description: 'Item 3 description', price: 30.0 },
+    },
+];
 </script>
-
 <template>
-  
+<ul>
+    <li v-for="item in items" :key="item.id">
+    <p>{{ item.name }}</p>
+    <p>{{ item.details.description }}</p>
+    <p>{{ item.details.price }}</p>
+    </li>
+</ul>
 </template>
-
-<style scoped>
-
-</style>
---
+++ aula07-RenderizacaoDeListas/Exercicios-RenderizacaodeListas/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import{ computed, ref } from 'vue';
+
+
+const cidade = ref('');
+const cidades = ref([
+  'São Paulo',
+  'Rio de Janeiro',
+  'Belo Horizonte',
+  'Salvador',
+  'Fortaleza',
+  'Curitiba',
+  'Manaus',
+  'Recife',
+  'Porto Alegre',
+  'Brasília',
+]);
+
+const cidadesOrdenadas = computed(() => [...cidades.value].sort()); // está criando uma cópia da lista (por isso ...) e ordenando. Computed serve para fazer isso automáticamente 
+
+function adicionarCidade() {
+  cidades.value.push(cidade.value);
+  cidade.value = '';
+
+}
+
+function removerCidade(index) {
+  cidades.value.splice(index, 1);
+}
+</script>
+
+<template>
+  <div></div>
+
+</template>
+
+<style scoped>
+
+</style>
