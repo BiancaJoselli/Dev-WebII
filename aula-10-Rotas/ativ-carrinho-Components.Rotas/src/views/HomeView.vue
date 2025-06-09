92241d4488ca020d000d3af157c4330636fb8263
BiancaJoselli <biancajoselli.ifc@gmail.com>
2025-06-09 12:13:42 -0300

    FEAT: Exercícios de rotas

---
+++ aula-10-Rotas/ativ-carrinho-Components.Rotas/src/views/HomeView.vue
@@ -0,0 +1,32 @@
+<script setup>
+import HeroComponent from '@/components/HeroComponent.vue'
+import CartComponent from '@/components/CartComponent.vue'
+import FeatureComponent from '@/components/FeatureComponent.vue'
+import BooksListing from '@/components/BooksListing.vue'
+
+import { useCartStore } from '@/store/cart.js'
+import { useBooksStore } from '@/store/books.js'
+const booksStore = useBooksStore()
+const cartStore = useCartStore()
+</script>
+
+<template>
+  <main v-if="cartStore.showCart">
+    <CartComponent
+      :cart="cartStore.cart"
+      @hide-cart="cartStore.showCart = false"
+      @increment-book="cartStore.incrementBookToCart"
+      @decrement-book="cartStore.decrementBookToCart"
+    />
+  </main>
+
+  <main v-else>
+    <HeroComponent></HeroComponent>
+
+    <FeatureComponent></FeatureComponent>
+
+    <BooksListing :books="booksStore.books" @add-to-cart="cartStore.addToCart"></BooksListing>
+  </main>
+
+  <FooterComponent></FooterComponent>
+</template>
---
+++ aula9-Rotas/ativ-carrinho-professor-Rotas/src/views/ReturnsView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>Devoluções</h1>
+</template>
